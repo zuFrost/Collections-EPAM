4cd36399f8b78354854ff821dfe9b29aa3b19710
Alex Frost <zufrost.a@gmail.com>
2020-05-01 23:57:53 +0300

    Make ArrayQueue

---
+++ scr/com/zufrost/learn/arraydeque/ArrayDequeMain.java
@@ -0,0 +1,24 @@
+package com.zufrost.learn.arraydeque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeMain {
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(11);
+        queue.offer(22);
+        queue.offer(33);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+
+    }
+}
