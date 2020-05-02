7824b3bca947313ac14ab76ce0d1d720e932868e
Alex Frost <zufrost.a@gmail.com>
2020-05-02 14:44:18 +0300

    Add HashSet and TreeSet

---
+++ scr/com/zufrost/learn/hashset/HashSetMain.java
@@ -0,0 +1,27 @@
+package com.zufrost.learn.hashset;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class HashSetMain {
+    public static void main(String[] args) {
+        HashSet<String> words = new HashSet<>(1000);
+        words.add("8Y1");
+        words.add("2Y");
+        words.add("2Y2");
+        words.add("8Y1");
+        words.add("6Y");
+        words.add("5Z");
+        words.add("4Y4");
+        System.out.println(words);
+        for (String element : words) {
+            System.out.print(element.hashCode() + " ");
+        }
+        System.out.println();
+        TreeSet<String> treeSet = new TreeSet<>(words);
+        System.out.println(treeSet);
+        for (String element : treeSet) {
+            System.out.print(element.hashCode() + " ");
+        }
+    }
+}
