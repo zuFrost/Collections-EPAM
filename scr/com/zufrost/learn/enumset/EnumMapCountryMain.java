bb17f6e0983541494f5925d317bb4f8d1815335d
Alex Frost <zufrost.a@gmail.com>
2020-05-02 16:07:36 +0300

    add EnumMapCountryMain

---
+++ scr/com/zufrost/learn/enumset/EnumMapCountryMain.java
@@ -0,0 +1,14 @@
+package com.zufrost.learn.enumset;
+
+import java.util.EnumMap;
+
+public class EnumMapCountryMain {
+    public static void main(String[] args) {
+        EnumMap<Country, Integer> map = new EnumMap<Country, Integer>(Country.class);
+        map.put(Country.POLAND, 8);
+        map.put(Country.UKRAINE, 1);
+        map.put(Country.BELARUS, 0);
+
+
+    }
+}
